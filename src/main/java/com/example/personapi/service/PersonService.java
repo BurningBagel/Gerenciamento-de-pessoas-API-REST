e946a2f3bfa5215b8d8854a3296ec12ee635ac16
BurningBagel <luiz.duarte11@yahoo.com.br>
2021-08-29 19:04:40 -0300

    Added DTO layer and Mapstruct dependency

--- src/main/java/com/example/personapi/service/PersonService.java
+++ src/main/java/com/example/personapi/service/PersonService.java
@@ -1,7 +1,9 @@
 package com.example.personapi.service;
 
 import com.example.personapi.dto.MessageResponseDTO;
+import com.example.personapi.dto.request.PersonDTO;
 import com.example.personapi.entity.Person;
+import com.example.personapi.mapper.PersonMapper;
 import com.example.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,13 +13,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class PersonService {
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person){
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+        Person personToSave = personMapper.toModel(personDTO);
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder().message("Created person with ID " + savedPerson.getId()).build();
     }
 }
